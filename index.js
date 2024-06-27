2b8da186bbb30f2ed2d30ab6ae17167f5b3d553e
Natalia Zelenevskaya <Zelenevskayan2015@gmail.com>
2024-06-27 17:03:07 +0300

    loader

---
+++ index.js
@@ -0,0 +1,8 @@
+let Loader = document.getElementById("loader");
+let Squirrel = document.getElementById("squirrel");
+
+Squirrel.onload = function HideLoader() {
+  Loader.style.display = "none";
+};
+
+HideLoader();
